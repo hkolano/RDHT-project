5fcc4ecc602c1ed8d9a1767f74aea16506d179b3
hkolano <hannah.kolano@students.olin.edu>
2021-05-11 11:52:56 -0700

    Create firstPassODE.m

---
+++ firstPassODE.m
@@ -0,0 +1,12 @@
+
+%% Set up parameters
+
+% Masses
+mp = 0.5;   % pulley mass, kg
+mpd = 0.1;  % Piston and diaphragm mass, kg
+mw = 0.1;   % Total mass of the water
+mhalfw = mw/2;   % Mass of half the water
+
+% Stiffnesses
+kp = 10000; % Stiffness of the belt (UNITS???)
+kh = 230;   % Stiffness of the hose (UNITS???)
