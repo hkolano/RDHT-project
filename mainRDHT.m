aeb8e5eacde080a5dba9ea30e08b8f637551f88c
parayiln <parayiln@oregonstate.edu>
2021-05-16 16:44:38 -0700

    added movement to motor

--- mainRDHT.m
+++ mainRDHT.m
@@ -12,25 +12,25 @@ close all
 %% Set up parameters
 
 % Geometry
-p.r = 0.0125; % radius of the pulley, in m
+p.r = 12.5/1000; % radius of the pulley, in m
 p.A1 = 0.0005; % Area of input piston, in m^2 (this is a little under a square inch)
 p.A2 = 0.0005; % Area of ouput piston, in m^2
 p.a = 0.00005; % Area of the tube, in m^2
 
 % Masses
-mp = 0.5;  % Pulley mass
+mp = 0.05;  % Pulley mass
 p.Ip = 0.5*mp*p.r^2;   % pulley inertia
 p.mpd = 0.05;  % Piston and diaphragm mass, kg
 p.mw = 0.1;   % Total mass of the water
 p.mw2 = p.mw/2;   % Mass of half the water
 
 % Stiffnesses
-p.kp = 1000; % Stiffness of the belt (UNITS???)
-p.kh = 5;   % Stiffness of the hose (UNITS???)
+p.kp = 100; % Stiffness of the belt 100 MPa 
+p.kh = 2;   % Stiffness of the hose (UNITS???)
 
 % Damping
-p.bp = .01;     % Damping of the belt 
-p.bf = 1;     % Viscous friction
+p.bp = .2;     % Damping of the belt 
+p.bf = .6;     % Viscous friction
 
 %% Simulate the system
 X0 = [0 0 0 0 0 0 0 0];
