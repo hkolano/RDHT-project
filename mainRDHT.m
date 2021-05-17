644b8f69ff48011b6b4fd3971979e7dba57de208
parayiln <parayiln@oregonstate.edu>
2021-05-17 00:26:05 -0700

    checked

--- mainRDHT.m
+++ mainRDHT.m
@@ -16,10 +16,11 @@ close all
 % Direct drive motor peak torque 19.6 Nm
 
 % Geometry
-p.r = 12.5/1000; % radius of the pulley, in m
-p.A1 = 0.0005; % Area of input piston, in m^2 (this is a little under a square inch)
-p.A2 = 0.0005; % Area of ouput piston, in m^2
-p.a = 0.00005; % Area of the tube, in m^2
+p.r = 23.9/1000; % radius of the pulley, in mm (from Design and Experiment...)
+p.A1 = (0.024/2)^2*pi; % Area of input piston, in m^2 (this is a little under a square inch)
+p.A2 = (0.024/2)^2*pi; % Area of ouput piston, in m^2
+p.a = (.006/2)^2*pi; % Area of the tube, in m^2
+p.strokelim =50;
 
 % Masses
 mp = 0.05;  % Pulley mass
@@ -29,33 +30,62 @@ p.mw = 0.1;   % Total mass of the water
 p.mw2 = p.mw/2;   % Mass of half the water
 
 % Stiffnesses
-p.kp = 100; % Stiffness of the belt 100 MPa
-p.kh = 2;   % Stiffness of the hose (UNITS???)
+p.kp = 10000; % Stiffness of the belt N/m (from paper sources)
+p.kh = 5;   % Stiffness of the hose (UNITS???)
+% p.kp = 50; % Stiffness of the belt (UNITS???)
+% p.kh = 1;   % Stiffness of the hose (UNITS???)
 
 % Damping
-p.bp = 0.1;     % Damping of the belt
-p.bf = 0.6;     % Viscous friction
+p.bp = .1;     % Damping of the belt
+p.bf = 1; %547;     % Viscous friction
 
 %% Simulate the system
 X0 = [0 0 0 0 0 0 0 0];
 
-[t_vec, X_vec, ~, mask] = simRDHT(X0,p);
+[t_vec, X_vec] = simRDHT(X0,p);
 
 %% Plotting
 figure
-plot(t_vec, X_vec(1,:))
+% plot(t_vec, X_vec(1,:))
+plot(t_vec, X_vec(:,1));
 hold on
-plot(t_vec, X_vec(3,:))
-% % plot(t_vec, 5*sin(t_vec))
-% legend('Theta1', 'Theta2', 'Force input')
+plot(t_vec, X_vec(:,7))
+% plot(t_vec, 5*sin(t_vec))
+legend('Theta1', 'Theta2')
+xlabel('Time (s)')
+ylabel('Radians')
+title('Angular Displacement')
+
+% figure
+% plot(t_vec, X_vec(:,2));
+% legend('D Theta 1')
+
+figure
+plot(t_vec, X_vec(:,3))
+hold on
+plot(t_vec, X_vec(:,5))
+legend('x1', 'x2')
+xlabel('Time (s)')
+ylabel('Displacement (m)')
+title('Piston Displacement')
 %
 % figure
-% plot(t_vec, X_vec(3,:))
-% hold on
-% plot(t_vec, X_vec(5,:))
-% legend('x1', 'x2')
-%
-% %Animate the mass
-% exportVideo = false;
-% playbackRate = .5;
-% RDHTAnimation(p,t_vec,X_vec,exportVideo,playbackRate);
+% plot(t_vec, X_vec(:,4))
+% legend('dx1')
+
+%Animate the mass
+figure
+plot(t_vec,X_vec(:,1),'g')
+hold on
+% plot(t_vec,X_vec(2,:),'-g')
+% plot(t_vec,X_vec(3,:),'b')
+% plot(t_vec,X_vec(4,:),'-b')
+% plot(t_vec,X_vec(5,:),'r')
+% plot(t_vec,X_vec(6,:),'-r')
+% plot(t_vec,X_vec(7,:),'y')
+% plot(t_vec,X_vec(8,:),'-y')
+
+%Animate
+exportVideo = false;
+playbackRate = 1;
+RDHTAnimation(p,t_vec,X_vec,exportVideo,playbackRate);
--- simRDHT.m
+++ simRDHT.m
@@ -1,19 +1,21 @@
-function [t_vec, X_vec, sol_set, mask] = simRDHT(X0,p)
-    %{
+
+  
+function [t_vec, X_vec] = simRDHT(X0,p) %  sol_set, mask] 
+    %{ 
     Simulation script for RDHT simulation
 
     Inputs:
         X0: vector of initial conditions
         p: structure with system parameters
-
+    
     Author: Andrew Peekema
     Modified: Hannah Kolano 2021
     %}
 
     % Running time
     t_start = 0;
-    t_end = 50;
-    dt = 0.5;
+    t_end = 10;
+    dt = 0.1;
 
     t_vec = t_start:dt:t_end;
     X_vec = zeros(length(X0), length(t_vec));
@@ -23,44 +25,54 @@ function [t_vec, X_vec, sol_set, mask] = simRDHT(X0,p)
     options = odeset(...
         'RelTol', 1e-9, ...
         'AbsTol', 1e-9);
-
+    
     % Bind dynamics function
     dynamics_fun = @(t,X)dyn(t,X,p);
+   
 
-    while t_start < t_end
+%     while t_start < t_end
         % Simulate the dynamics over a time interval
         % Kept framework for hybrid dynamics
 %         if p.state == 1
-            sol = ode45(dynamics_fun, [t_start,t_end], X0, options);
+      [t_vec, X_vec] = ode45(dynamics_fun, [t_start,t_end], X0, options);
+%             disp('Contact! at t = ')
+%             disp(sol.x(end))
+%             p.state = 0;
+%         else 
+%             sol = ode45(conn_dyn, [t_start,t_end], X0, contactoptions);
+%             disp('Disconnected! at t = ')
+%             disp(sol.x(end))
+%             p.state = 1;
+%         end
 
-
-        sol_set = [sol_set, {sol}];
-        % Setup t_start for the next ode45 call so it is at the end of the
-        % last call
-        t_start = sol.x(end);
-        % Set the initial conditions to the end of the last run
-        if t_start == t_end
-            X0 = [0 0 0 0 0 0 0 0];
-        else
-            X0 = sol.ye(:,end);
-        end
-    end
-
-    % Loop to sample the solution structures and built X_vec
-    mask = zeros(1,length(t_vec));
-    for idx = 1:length(sol_set)
-        % This sets up a logical vector so we can perform logical indexing
-        t_sample_mask = t_vec >= sol_set{idx}.x(1) & t_vec <= sol_set{idx}.x(end);
-        % Evaluate the idx solution structure only at the applicable times
-        X_eval = deval(sol_set{idx}, t_vec(t_sample_mask));
-        % Assign the result to the correct indicies of the return state array
-        X_vec(:,t_sample_mask) = X_eval;
-
-        if rem(idx,2) == 0
-            mask(t_sample_mask) = t_sample_mask(t_sample_mask);
-        end
-    end
-    mask = logical(mask);
+        % Concatenate solution sets
+%         sol_set = [sol_set, {sol}];
+%         % Setup t_start for the next ode45 call so it is at the end of the 
+%         % last call 
+%         t_start = sol.x(end);
+%         % Set the initial conditions to the end of the last run
+%         if t_start == t_end
+%             X0 = [0 0 0 0 0 0 0 0];
+%         else
+%             X0 = sol.ye(:,end);
+%         end
+%     end
+%     
+%     % Loop to sample the solution structures and built X_vec
+%     mask = zeros(1,length(t_vec));
+%     for idx = 1:length(sol_set)
+%         % This sets up a logical vector so we can perform logical indexing
+%         t_sample_mask = t_vec >= sol_set{idx}.x(1) & t_vec <= sol_set{idx}.x(end);
+%         % Evaluate the idx solution structure only at the applicable times
+%         X_eval = deval(sol_set{idx}, t_vec(t_sample_mask));
+%         % Assign the result to the correct indicies of the return state array
+%         X_vec(:,t_sample_mask) = X_eval;
+%         
+%         if rem(idx,2) == 0
+%             mask(t_sample_mask) = t_sample_mask(t_sample_mask);
+%         end
+%     end
+%     mask = logical(mask);
 
 end % simRDHT
 
@@ -68,16 +80,17 @@ function dX = dyn(t,X,p)
     % t == time
     % X == the state
     % p == parameters structure
-   fq=2;
-   a=5;
-    Tau_in = -a*fq*fq*cos(t*fq)
-
+    Tau_in = .25*cos(t);
+%     Tau_in = 0;
+    
     M1 = p.mw2*p.A1/p.a + p.mpd*p.a/p.A1;
     M2 = p.mw2*p.A2/p.a + p.mpd*p.a/p.A2;
     kpaOVA1  = p.kp*p.a/p.A1;
     bpaOVA1  = p.bp*p.a/p.A1;
     kpaOVA2  = p.kp*p.a/p.A2;
     bpaOVA2  = p.bp*p.a/p.A2;
+    
+    k = pistonLimitSpring(X(3), p);
 
     A = [0                 1                   0                           0                           0                           0                           0                   0; ...
         -p.kp*p.r^2/p.Ip     -p.bp*p.r^2/p.Ip    p.kp*p.r/p.Ip             p.bp*p.r/p.Ip               0                           0                           0                   0; ...
@@ -88,13 +101,78 @@ function dX = dyn(t,X,p)
         0                   0                   0                           0                           0                           0                           0                   1; ...
         0                   0                   0                           0                           p.kp*p.r/p.Ip            p.bp*p.r/p.Ip                -p.kp*p.r^2/p.Ip     -p.bp*p.r^2/p.Ip];
 %      A = [0                 1                   0                           0                           0                           0                           0                   0; ...
-%         -p.kp*p.r^2/p.Ip     -p.bp*p.r^2/p.Ip    p.kp*p.r/p.Ip             p.bp*p.r/p.Ip               0                           0                            0                   0; ...
-%         0                   0                   0                           1                           0                           0                           0                   0; ...
-%         p.kp*p.r/p.mpd      p.bp*p.r/p.mpd      -p.kp/p.mpd                 -p.bp/p.mpd                 0                           0                           0                   0; ...
+%         -p.kp*p.r^2/p.Ip     -p.bp*p.r^2/p.Ip    p.kp*p.r/p.Ip     p.bp*p.r/p.Ip               0                           0                            0                   0; ...
+%         0                   0                   0                  1                           0                           0                           0                   0; ...
+%         p.kp*p.r/p.mpd      p.bp*p.r/p.mpd      -p.kp/p.mpd        -p.bp/p.mpd                 0                           0                           0                   0; ...
 %         0                   0                   0                           0                           0                           1                           0                   0; ...
 %         0                   0                   0                           0                           0                           0                           0                   0; ...
 %         0                   0                   0                           0                           0                           0                           0                   1; ...
 %         0                   0                   0                           0                           0                           0                           0                   0];
-
-dX = A*X + [0; Tau_in; 0; 0; 0; 0; 0; 0];
+    dX = A*X + [0; Tau_in/p.Ip; 0; 0; 0; 0; 0; 0];
 end % dynamics
+
+function k = pistonLimitSpring(x, p)
+    if x > p.strokelim
+        k = 100000;
+    elseif x < -p.strokelim
+        k = -100000;
+    else
+        k = 0;
+    end
+end
+
+%% Hybrid functions
+% function dX = connecteddynamics(t,X,p,ctlr_fun)
+%     % t == time
+%     % X == the state
+%     % p == parameters structure
+%     F_ctrl = ctlr_fun(t,X);
+%     
+%     y_act  = X(1); % Position of actuator
+%     y_load = X(2); % Position of load
+%     dy_act = X(3); % Velocity of actuator
+%     dy_load = X(4); % Velocity of load
+% 
+%     % Return the state derivative
+%     dX = zeros(4,1);
+%     dX(1) = dy_act;     % Velocity of actuator
+%     dX(2) = dy_load;    % Velocity of load
+%     dX(3) = (F_ctrl + p.k*(y_load-y_act) + p.c*(dy_load-dy_act))/p.actm;          % Acceleration of actuator
+%     dX(4) = ( - p.k*(y_load-y_act) - p.c*(dy_load-dy_act))/p.loadm; % Acceleration of load
+% end % dynamics
+% 
+% function [eventVal, isterminal, direction] = contactSpringEvent(t,X,p)
+% % halting event function for ODE simulation. Events are distance to
+%     % ceiling and distance to paddle
+%     % Inputs
+%     % t: time, X: the state, p: parameters structure
+%     % Outputs
+%     % eventVal: Vector of event functions that halt at zero crossings
+%     % isterminal: if the simulation should halt (yes for both)
+%     % direction: which direction of crossing should the sim halt (positive)
+%     equil = X(2)-X(1); % distance between load and actuator, 0 when spring is at equilibrium
+%     
+%     eventVal = equil;   % when spring at equilibrium distance...
+%     isterminal = 1;     % stops the sim
+%     direction = 0;      % any direction
+% end
+% 
+% function [eventVal, isterminal, direction] = liftoffSpringEvent(t,X,p)
+% % halting event function for ODE simulation. Events are distance to
+%     % ceiling and distance to paddle
+%     % Inputs
+%     % t: time, X: the state, p: parameters structure
+%     % Outputs
+%     % eventVal: Vector of event functions that halt at zero crossings
+%     % isterminal: if the simulation should halt (yes for both)
+%     % direction: which direction of crossing should the sim halt (positive)
+%     y_act  = X(1); % Position of actuator
+%     y_load = X(2); % Position of load
+%     dy_act = X(3); % Velocity of actuator
+%     dy_load = X(4); % Velocity of load
+%    
+%     eventVal =  - p.k*(y_load-y_act) - p.c*(dy_load-dy_act);   % when force from spring and damper = 0...
+%     isterminal = 1;     % stops the sim
+%     direction = -1;      % doesn't matter which direction
+% end
+
