eeec2e017c353009b6a9b860b11d6cab0e6fed32
hkolano <hannah.kolano@students.olin.edu>
2021-05-30 20:09:52 -0700

    Ball bounces against floor and rod

--- mainRDHTballbounce.m
+++ mainRDHTballbounce.m
@@ -21,14 +21,21 @@ p.A2 = (0.024/2)^2*pi; % Area of ouput piston, in m^2
 p.a = (.006/2)^2*pi; % Area of the tube, in m^2
 p.strokelim = 56.8/2/1000; % Stroke length limit (either way from 0)
 
+p.h = 0.5; % Distance between pulley center and ground
+
 % Masses
 mp = 0.05;  % Pulley mass
 p.Ip = 0.5*mp*p.r^2;   % pulley inertia
 p.mpd = 0.05;  % Piston and diaphragm mass, kg
 p.mw = 0.1;   % Total mass of the water
 p.mw2 = p.mw/2;   % Mass of half the water
-
 p.mball = 0.058; % tennis ball weighs 58g
+
+rad_rod = 0.01; % radius of rod: 1 cm
+p.l_rod = 10*p.r; % length of rod is 10x radius of pulley
+vol_rod = pi*(rad_rod^2-(rad_rod-0.002)^2)*p.l_rod; % pi*(r_outer^2 - r_inner^2)*length
+p.mrod = vol_rod*2700;
+p.Irod = p.mrod*p.l_rod^2/3;
 
 % Stiffnesses
 p.kp = 2014000; % Stiffness of the belt N/m 
@@ -40,7 +47,7 @@ p.bp = 100;     % Damping of the belt
 p.bf = 2.137;     % Viscous friction N/(m/s) of y1
 
 % Torque curve
-p.freq = 1;
+p.freq = 2;
 
 % External disturbance
 % p.dist_amp = .5; % Amplitude of disturbance: ~30 degrees
@@ -58,7 +65,7 @@ p.freq = 1;
 % ctlr_fun = @(t,X) ctlrRDHTforce(t,X,c,p,tau_des, traj_fun);
 
 %% Simulate the system
-X0 = [.5 0 .5*p.r 0 .5*p.r 0 .5 0, 1, 0];
+X0 = [.5 0 .5*p.r 0 .5*p.r 0 .5 0, 1.5, 0];
 
 [t_vec, X_vec] = simRDHTballBounce(X0,p);
 
@@ -73,14 +80,14 @@ X0 = [.5 0 .5*p.r 0 .5*p.r 0 .5 0, 1, 0];
 
 %% Retrieve output forces
 % [~, ~, ddy] = traj_fun(t_vec);
-system_state = X_vec(1:8,:);
-tau_out = [0  0   0   0   p.kp*p.r    p.bp*p.r   -p.kp*p.r^2   -p.bp*p.r^2]*system_state;
-% output_tau = calculate_output_torque(t_vec, X_vec, traj_fun)
-figure
-plot(t_vec, tau_out,  'b-', 'LineWidth', 2)
-ylabel('Output Force (Nm)')
-xlabel('Time (s)')
-title('Torque on Output')
+% system_state = X_vec(1:8,:);
+% tau_out = [0  0   0   0   p.kp*p.r    p.bp*p.r   -p.kp*p.r^2   -p.bp*p.r^2]*system_state;
+% % output_tau = calculate_output_torque(t_vec, X_vec, traj_fun)
+% figure
+% plot(t_vec, tau_out,  'b-', 'LineWidth', 2)
+% ylabel('Output Force (Nm)')
+% xlabel('Time (s)')
+% title('Torque on Output')
 
 
 %% Plotting
